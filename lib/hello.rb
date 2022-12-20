0f30a372b0a66345748894a55fb72e12a2ee903a
Josef Šimánek <josef.simanek@gmail.com>
2022-12-20 21:04:49 +0100

    Init

---
+++ lib/hello.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "hello/version"
+require_relative "hello/hello"
+
+module Hello
+  class Error < StandardError; end
+  # Your code goes here...
+end
